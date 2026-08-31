94eaaed2a6e03bf8620c876d531b2d2b400791af
cni-works <torans7010@gmail.com>
2026-08-31 15:55:59 +0900

    Release CNI Blocks v1.40.0

--- cni_blocks.php
+++ cni_blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: cni_blocks
  * Description: A small block pack with gallery and flexible container blocks.
- * Version: 1.4.0
+ * Version: 1.40.0
  * Requires at least: 6.3
  * Requires PHP: 7.4
  * Update URI: https://github.com/cni-works/cni_blocks
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === cni_blocks ===
 Requires at least: 6.3
 Requires PHP: 7.4
-Stable tag: 1.4.0
+Stable tag: 1.40.0
 
 Includes:
 - Slide Gallery block (cni-blocks/slide-gallery)
